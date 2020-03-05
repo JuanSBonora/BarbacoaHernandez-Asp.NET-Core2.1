28495c6f0612db87d435d1397b35d0e52261f43b
JuanSBonora <juanfcosantiagob1002@outlook.com>
2020-03-04 23:32:27 -0600

    scaffolding -> Pages/Account/Login.cshtml

---
+++ ProyBarbacoaHernandez/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using ProyBarbacoaHernandez.Data;
+
+[assembly: HostingStartup(typeof(ProyBarbacoaHernandez.Areas.Identity.IdentityHostingStartup))]
+namespace ProyBarbacoaHernandez.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+            });
+        }
+    }
+}
