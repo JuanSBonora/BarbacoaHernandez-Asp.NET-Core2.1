468d1dc61035e045ff7450d4f46b542498859ecc
JuanSBonora <juanfcosantiagob1002@outlook.com>
2020-03-14 01:44:20 -0600

    process for obtain role of user

---
+++ ProyBarbacoaHernandez/Library/ListObject.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using ProyBarbacoaHernandez.Data;
+using ProyBarbacoaHernandez.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ProyBarbacoaHernandez.Library
+{
+    public class ListObject
+    {
+        public String description, code; 
+
+        public UsersRoles _usersRole;
+        public UserData _userData;
+        public IdentityError _identityError;
+        public ApplicationDbContext _context;
+
+
+        public List<SelectListItem> _userRoles;
+
+        public RoleManager<IdentityRole> _roleManager;
+        public UserManager<IdentityUser> _userManager;
+        public SignInManager<IdentityUser> _signInManager;
+
+        public List<object[]> dataList = new List<object[]>();
+    }
+}
---
+++ ProyBarbacoaHernandez/Library/Usuarios.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ProyBarbacoaHernandez.Library
+{
+    public class Usuarios : ListObject
+    {
+        public Usuarios()
+        {
+
+        }
+        public Usuarios(RoleManager<IdentityRole> roleManager)
+        {
+            _roleManager = roleManager;
+            _usersRole = new UsersRoles();
+        }
+        public Usuarios(UserManager<IdentityUser> userManager, SignInManager<IdentityUser> signInManager, RoleManager<IdentityRole> roleManager)
+        {
+            _userManager = userManager;
+            _roleManager = roleManager;
+            _signInManager = signInManager;
+            _usersRole = new UsersRoles();
+        }
+        internal async Task<List<object[]>> userLogin(string email, string password)
+        {
+            try
+            {
+                var result = await _signInManager.PasswordSignInAsync(email, password, false, lockoutOnFailure: false);
+                if (result.Succeeded)
+                {
+                    var appUser = _userManager.Users.Where(u => u.Email.Equals(email)).ToList();
+
+                }
+                else
+                {
+                    code = "1";
+                    description = "incorrect e-mail address or password";
+                }
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+        }
+    }
+}
