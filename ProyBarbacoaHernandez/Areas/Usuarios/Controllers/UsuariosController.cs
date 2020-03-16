f557f20f27c16f6f67e098b1fd509ae3e3618f77
JuanSBonora <juanfcosantiagob1002@outlook.com>
2020-03-16 00:57:29 -0600

    security & cookies

--- ProyBarbacoaHernandez/Areas/Usuarios/Controllers/UsuariosController.cs
+++ ProyBarbacoaHernandez/Areas/Usuarios/Controllers/UsuariosController.cs
@@ -2,15 +2,27 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using ProyBarbacoaHernandez.Controllers;
 
 namespace ProyBarbacoaHernandez.Areas.Usuarios.Controllers
 {
     public class UsuariosController : Controller
     {
+        private readonly SignInManager<IdentityUser> _signInManager;
+        public UsuariosController(SignInManager<IdentityUser> signInManager)
+        {
+            _signInManager = signInManager;
+        }
         public IActionResult Index()
         {
             return View();
+        }
+        public async Task<IActionResult> SessionClose()
+        {
+            await _signInManager.SignOutAsync();
+            return RedirectToAction(nameof(HomeController.Index), "Home");
         }
     }
 }
