08f093e65dc9240b0e05536b5def1ee3a9a8928c
JuanSBonora <juanfcosantiagob1002@outlook.com>
2020-05-02 03:25:32 -0500

    Obteniendo los errores generados en el servidor

--- ProyBarbacoaHernandez/Areas/Usuarios/Pages/Registrar/Registrar.cshtml.cs
+++ ProyBarbacoaHernandez/Areas/Usuarios/Pages/Registrar/Registrar.cshtml.cs
@@ -19,13 +19,15 @@ namespace ProyBarbacoaHernandez.Areas.Usuarios.Pages.Registrar
     {
         private ListObject objeto = new ListObject();
         //private LUsuarios _usuarios;
-        public RegistrarModel(RoleManager<IdentityRole> roleManager, IHostingEnvironment environment) {
+        public RegistrarModel(RoleManager<IdentityRole> roleManager,UserManager<IdentityUser> userManager, IHostingEnvironment environment) {
             objeto._roleManager = roleManager;
+            objeto._userManager = userManager;
             objeto._environment = environment;
             objeto._usuarios = new LUsuarios();
             objeto._usersRole = new UsersRoles();
             objeto._image = new Uploadimage();
-        }
+            objeto._userRoles = new List<SelectListItem>();
+    }
         public void OnGet() {
             Input = new InputModel
             {
@@ -37,6 +39,8 @@ namespace ProyBarbacoaHernandez.Areas.Usuarios.Pages.Registrar
         public class InputModel : InputModelRegistrar {
             [Required]
             public string Role { get; set; }
+            [TempData]
+            public string ErrorMessage { get; set; }
             public IFormFile AvatarImage { get; set; }
             public List<SelectListItem> rolesLista { get; set; }
         }
@@ -49,13 +53,33 @@ namespace ProyBarbacoaHernandez.Areas.Usuarios.Pages.Registrar
         {
             try
             {
+                objeto._userRoles.Add(new SelectListItem
+                {
+                    Text = Input.Role
+                });
+                var userList = objeto._userManager.Users.Where(u => u.Email.Equals(Input.Email)).ToList();
+                if (userList.Count.Equals(0))
+                {
+
+                }
+                else
+                {
+                    Input = new InputModel
+                    {
+                        ErrorMessage = "El " + Input.Email + " ya esta registrado",
+                        rolesLista = objeto._userRoles
+                    };
+                }
                 var imageName = Input.Email + ".png";
                 await objeto._image.copiarImagenAsync(Input.AvatarImage, imageName, objeto._environment, "Usuarios");
             }
             catch (Exception ex)
             {
-
-                throw;
+                Input = new InputModel
+                {
+                    ErrorMessage = ex.Message,
+                    rolesLista = objeto._userRoles
+                };
             }
         }
     }
